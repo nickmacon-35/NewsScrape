306d12f46fa3bb867567ae2dc21e5aee88acaa1f
Nick Macon <nickmacon@gmail.com>
2020-02-15 14:26:16 -0600

    2.15 14:26 push

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -29,7 +29,9 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
+
+// , { useNewUrlParser: true }
 // Routes
 
 // A GET route for scraping the echoJS website
